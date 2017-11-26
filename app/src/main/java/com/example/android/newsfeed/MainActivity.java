b37a082a05e32e7af54503587c0b47d84efbd1d6
doc <sialtek@gmail.com>
2017-11-26 20:33:47 +0000

    Implemented SwipeRefreshLayout and attribution

--- app/src/main/java/com/example/android/newsfeed/MainActivity.java
+++ app/src/main/java/com/example/android/newsfeed/MainActivity.java
@@ -8,8 +8,12 @@ import android.content.Loader;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -20,19 +24,22 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements LoaderCallbacks<List<News>> {
+public class MainActivity extends AppCompatActivity implements LoaderCallbacks<List<News>>, SwipeRefreshLayout.OnRefreshListener{
 
+    LoaderManager loaderManager;
     private static final int NEWS_LOADER_ID = 1;
     NewsAdapter newsAdapter;
     TextView emptyView;
     ProgressBar progressBar;
     private static final String NEWS_REQUEST_URL = "https://content.guardianapis.com/search?api-key=***REMOVED***e417";
     private String queryString = "";
+    SwipeRefreshLayout swipeRefreshLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
+        swipeRefreshLayout.setOnRefreshListener(this);
         queryString = NEWS_REQUEST_URL;
         // Check network connection
         ConnectivityManager cm =
@@ -58,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
         }{
 
             // Get a reference to the LoaderManager, in order to interact with loaders.
-            LoaderManager loaderManager = getLoaderManager();
+            loaderManager = getLoaderManager();
 
             // Initialize the loader. Pass in the int ID constant defined above and pass in null for
             // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
@@ -67,32 +74,6 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
 
             // Instance of NewsAdapter
             // Create a new {@link ArrayAdapter} of news
-            newsAdapter = new NewsAdapter(this, new ArrayList<News>());
-
-//            List<News> news = new ArrayList<>();
-//            news.add(new News("F1: Valtteri Bottas wins the season-ending Abu Dhabi GP â€“ live!", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("F1: Valtteri Bottas wins the season-ending Abu Dhabi GP â€“ live!", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("F1: Valtteri Bottas wins the season-ending Abu Dhabi GP â€“ live!", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("F1: Valtteri Bottas wins the season-ending Abu Dhabi GP â€“ live!", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("We need new words to explain these curious times. How about â€˜coffusedâ€™ or â€˜procrastinetflixâ€™? | Arwa Mahdawi", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("We need new words to explain these curious times. How about â€˜coffusedâ€™ or â€˜procrastinetflixâ€™? | Arwa Mahdawi", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("We need new words to explain these curious times. How about â€˜coffusedâ€™ or â€˜procrastinetflixâ€™? | Arwa Mahdawi", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("We need new words to explain these curious times. How about â€˜coffusedâ€™ or â€˜procrastinetflixâ€™? | Arwa Mahdawi", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("We need new words to explain these curious times. How about â€˜coffusedâ€™ or â€˜procrastinetflixâ€™? | Arwa Mahdawi", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("We need new words to explain these curious times. How about â€˜coffusedâ€™ or â€˜procrastinetflixâ€™? | Arwa Mahdawi", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("Footbal", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("Footbal", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("Footbal", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("Footbal", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("Footbal", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("Footbal", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("Footbal", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("Footbal", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("Footbal", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("Footbal", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-//            news.add(new News("Footbal", "Sport", "Ciccio Formaggio", "2017-11-26T15:07:26Z"));
-
-
             newsAdapter = new NewsAdapter(this, new ArrayList<News>());
 
             // Set the adapter on the {@link ListView}
@@ -115,6 +96,13 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
     public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
         // Create a new loader for the given URL
         return new NewsLoader(this, queryString);
@@ -123,6 +111,7 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
     @Override
     public void onLoadFinished(Loader<List<News>> loader, List<News> news) {
         // Clear the adapter of previous data
+        swipeRefreshLayout.setRefreshing(false);
         newsAdapter.clear();
         String empty_list = getString(R.string.empty_list);
         emptyView.setText(empty_list);
@@ -140,5 +129,44 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
     public void onLoaderReset(Loader<List<News>> loader) {
         // Loader reset, so we can clear out our existing data.
         newsAdapter.clear();
+    }
+
+    @Override
+    public void onRefresh() {
+
+        // This method performs the actual data-refresh operation.
+        // The method calls setRefreshing(false) when it's finished.
+        refreshNews();
+    }
+
+    private void refreshNews(){
+        Log.v("Update called", "New refresh request");
+        loaderManager.restartLoader(NEWS_LOADER_ID, null, this);
+    }
+    /*
+ * Listen for option item selections so that we receive a notification
+ * when the user requests a refresh by selecting the refresh action bar item.
+ */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            // Check if user triggered a refresh:
+            case R.id.menu_refresh:
+                Log.v("", "Refresh menu item selected");
+
+                // Signal SwipeRefreshLayout to start the progress indicator
+                swipeRefreshLayout.setRefreshing(true);
+
+                // Start the refresh background task.
+                // This method calls setRefreshing(false) when it's finished.
+                refreshNews();
+
+                return true;
+        }
+
+        // User didn't trigger a refresh, let the superclass handle this action
+        return super.onOptionsItemSelected(item);
+
     }
 }
